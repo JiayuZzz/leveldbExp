dfeb2ab53ae9ca829f18e3e20d70d70000ae2913
yuwolf <wujy.cs@gmail.com>
2019-08-06 01:26:59 +0000

    upadte table size

--- util/options.cc
+++ util/options.cc
@@ -21,7 +21,7 @@ Options::Options()
       block_cache(nullptr),
       block_size(4096),
       block_restart_interval(16),
-      max_file_size(64<<20),
+      max_file_size(16<<20),
       compression(kSnappyCompression),
       reuse_logs(false),
       filter_policy(nullptr) {
